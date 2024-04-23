c1f243a32a21d3477ef1b4eb5247425e20be6627
Dila <dila.karadag1812@gmail.com>
2024-04-23 22:08:38 +0300

    ..

---
+++ vektorel_1904/okultakipsistemi.py
@@ -0,0 +1,10 @@
+import sqlite3
+veriTabanı = sqlite3.connect('okultakipsistemi.db') # varsa bağlan, yoksa oluştur.
+secilenvt = veriTabanı.cursor()
+
+def tabloOlustur():
+    secilenvt.execute("CREATE TABLE ogrenciler(adSoyad, numara) ")
+    veriTabanı.commit()
+    veriTabanı.close()
+
+tabloOlustur()
